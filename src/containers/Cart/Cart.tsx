d978920b202c3fe0ac45160bb7a748452408f3c0
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-21 18:31:33 +0600

    Cart updated: now displays a message if the cart is empty

--- src/containers/Cart/Cart.tsx
+++ src/containers/Cart/Cart.tsx
@@ -9,12 +9,18 @@ interface CartProps {
 const Cart: FC<CartProps> = ({ items }) => {
   return (
     <div>
-      <h3>Ваша корзина:</h3>
-      <ul className='list-group'>
-        {items.map((x, i) => (
-          <CartItem key={i} item={x} />
-        ))}
-      </ul>
+      {items.length ? (
+        <>
+          <h3>Ваша корзина:</h3>
+          <ul className='list-group'>
+            {items.map((x, i) => (
+              <CartItem key={i} item={x} />
+            ))}
+          </ul>
+        </>
+      ) : (
+        <h3>Ваша корзина пуста</h3>
+      )}
     </div>
   );
 };
