a3286e8ba369bc3af070cb359c904711da041177
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-21 16:15:07 +0600

    constants added

---
+++ src/constants.ts
@@ -0,0 +1,20 @@
+import { ContactInfo } from './types';
+
+export const contacts: ContactInfo[] = [
+  { id: 0, title: 'мкрн. Кок Жар', address: 'мкрн. Кок-Жар, 5/1', deliveryHours: 'Пн-Вс: 08:00 — 01:00', openHours: 'Пн-Вс: 08:00 — 01:00' },
+  { id: 0, title: '12 мкр, ДК', address: 'ул. Нуркамал Жетикашкаевой, 29', deliveryHours: 'Пн-Вс: 08:00 — 01:00', openHours: 'Пн-Вс: 08:00 — 01:00' },
+  {
+    id: 0,
+    title: 'Байтик Баатыра 96',
+    address: 'ул. Байтик Баатыра (Советская), 96',
+    deliveryHours: 'Пн-Вс: 08:00 — 01:00',
+    openHours: 'Пн-Вс: 08:00 — 01:00',
+  },
+  { id: 0, title: 'Восток-5', address: 'пр-т Чуйский , 32Б', deliveryHours: 'Пн-Вс: 08:00 — 01:00', openHours: 'Пн-Вс: 08:00 — 01:00' },
+  { id: 0, title: 'Киркомстром', address: 'ул. Фрунзе (село Новопавловка), 37', deliveryHours: 'Пн-Вс: 08:00 — 01:00', openHours: 'Пн-Вс: 08:00 — 01:00' },
+  { id: 0, title: 'Манаса 7, ББ', address: 'пр-т Манаса , 7', deliveryHours: 'Пн-Вс: 08:00 — 23:00', openHours: 'Пн-Вс: 08:00 — 23:00' },
+  { id: 0, title: 'ТРЦ "Ала-Арча"', address: 'пр-т Чингиза Айтматова, 299в', deliveryHours: 'Пн-Вс: 08:00 — 01:00', openHours: 'Пн-Вс: 10:00 — 23:00' },
+  { id: 0, title: 'ТРЦ «I-Mall»', address: 'пер. Шевченко, 80', deliveryHours: 'Пн-Вс: Круглосуточно', openHours: 'Пн-Вс: Круглосуточно' },
+  { id: 0, title: 'Шопокова 101/1', address: 'ул. Д. Шопокова, 101/1', deliveryHours: 'Пн-Вс: 08:00 — 01:00', openHours: 'Пн-Вс: 08:00 — 01:00' },
+  { id: 0, title: 'Юнусалиева 179/2', address: 'ул. Б. Юнусалиева , 179/2', deliveryHours: 'Пн-Вс: Круглосуточно', openHours: 'Пн-Вс: Круглосуточно' },
+];
