57da053562d3ad8018577844ff47ef51ccef5976
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-21 17:38:21 +0600

    Home updated

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,5 +1,21 @@
-const Home = () => {
-  return <div></div>;
+import { FC } from 'react';
+import DishCard from '../../components/DishCard/DishCard';
+import { DishInfo } from '../../types';
+
+interface HomeProps {
+  dishes: DishInfo[];
+}
+
+const Home: FC<HomeProps> = ({ dishes }) => {
+  return (
+    <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-2'>
+      {dishes.map((x) => (
+        <div className='col'>
+          <DishCard key={x.id} info={x} />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Home;
