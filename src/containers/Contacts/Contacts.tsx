c8f3da8eef5578bb3aec2cbc72511b7fe47f78f6
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-21 18:41:18 +0600

    Contacts updated: 'key' bug fixed

--- src/containers/Contacts/Contacts.tsx
+++ src/containers/Contacts/Contacts.tsx
@@ -10,8 +10,8 @@ const Contacts: FC<ContactsProps> = ({ contacts }) => {
   return (
     <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-2'>
       {contacts.map((x) => (
-        <div className='col'>
-          <ContactCard key={x.id} info={x} />
+        <div key={x.id} className='col'>
+          <ContactCard info={x} />
         </div>
       ))}
     </div>
