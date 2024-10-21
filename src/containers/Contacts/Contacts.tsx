dfdf4e7bac56cd813f9798a2d616c1c39965d07b
Anatoly N <16267644+miloradowicz@users.noreply.github.com>
2024-10-21 16:14:32 +0600

    Contacts components added

---
+++ src/containers/Contacts/Contacts.tsx
@@ -0,0 +1,21 @@
+import { FC } from 'react';
+import { ContactInfo } from '../../types';
+import ContactCard from '../../components/ContactCard/ContactCard';
+
+interface ContactsProps {
+  contacts: ContactInfo[];
+}
+
+const Contacts: FC<ContactsProps> = ({ contacts }) => {
+  return (
+    <div className='row row-cols-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-2'>
+      {contacts.map((x) => (
+        <div className='col'>
+          <ContactCard key={x.id} contactInfo={x} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Contacts;
